7eb5ea9f38d00eea3549ae68316ecbfaa0350cff
kenneth Roman <kimkhay8@gmail.com>
2023-08-21 19:35:47 +0800

    f-commit

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,54 @@
+
+"use client"
+
+import React, { useState } from 'react'
+
+
+import '../styles/nav.css'
+
+const Nav = () => {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+  const handleToggleNavbar = () => {
+    setIsNavbarOpen(!isNavbarOpen);
+  };
+
+  return (
+    <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-body" data-bs-theme="dark">
+      <div className="container-fluid d-flex align-items-between">
+        <a className="navbar-brand" href="#">
+          <h2>
+            <b>WHITECLOCK</b>
+          </h2>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={handleToggleNavbar}
+        >
+          <span className=" d-flex navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className={`collapse navbar-collapse   ${isNavbarOpen ? ' show' : ''}`}
+          id="navbarScroll"
+        >
+         <div className="navbar-nav    text-center">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <a className="nav-link" href="#">Features</a>
+            <a className="nav-link" href="#">Pricing</a>
+            <a className="nav-link  disabled" aria-disabled="true">Disabled</a>
+          </div>
+
+
+        </div>
+
+       
+
+       
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/app/components/banner-top.tsx
@@ -0,0 +1,14 @@
+import "../styles/banner-top.css"
+
+const BannerTop = () => {
+
+    return (
+        <div className="container banner-top mt-3 py-5">
+            <h1 className="display-5 fw-bold text-white">Empowering Innovation through Custom Software Development and IT Augmentation Services</h1>
+        </div>
+    )
+
+}
+
+
+export default BannerTop;
