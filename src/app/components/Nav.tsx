7e5e64ce82f277bca350984eed12467430678767
kenneth Roman <kimkhay8@gmail.com>
2023-08-30 21:21:15 +0800

    none

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -14,30 +14,30 @@ const Nav = () => {
   };
 
   return (
-    <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-body" data-bs-theme="dark">
+    <nav className="navbar sticky-top navbar-expand-lg  " data-bs-theme="dark">
       <div className="container-fluid d-flex align-items-between">
         <a className="navbar-brand" href="#">
-          <h2>
-            <b>WHITECLOCK</b>
-          </h2>
+          <h1>
+            <b className='text-dark'>WHITECLOCK</b>
+          </h1>
         </a>
         <button
-          className="navbar-toggler"
+          className="navbar-toggler bg-dark"
           type="button"
           onClick={handleToggleNavbar}
         >
-          <span className=" d-flex navbar-toggler-icon"></span>
+          <span className=" d-flex navbar-toggler-icon "></span>
         </button>
 
         <div
           className={`collapse navbar-collapse   ${isNavbarOpen ? ' show' : ''}`}
           id="navbarScroll"
         >
-         <div className="navbar-nav    text-center">
-            <a className="nav-link active wc-nav fs-5 fw-strong" aria-current="page" href="#">Home</a>
-            <a className="nav-link wc-nav fs-5 fw-strong" href="#">Features</a>
-            <a className="nav-link wc-nav fs-5 fw-strong" href="#">Pricing</a>
-            <a className="nav-link wc-nav fs-5 fw-strong  disabled" aria-disabled="true">Disabled</a>
+         <div className="navbar-nav text-center position-relative">
+            <a className="nav-link fs-3 fw-bold text-dark p-3" aria-current="page" href="#">ABOUT US</a>
+            <a className="nav-link fs-3 fw-bold text-dark p-3" href="#">SERVICES</a>
+            <a className="nav-link fs-3 fw-bold text-dark p-3" href="#">WORK</a>
+            <a className="nav-link fs-3 fw-bold text-dark p-3 disabled" aria-disabled="true">INSIGHTS</a>
           </div>
 
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import type { Metadata } from 'next'
 
